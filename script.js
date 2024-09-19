28b41cb0321fd372f229e23a9f854c8c176456ef
Patryk Bielecki <bielecki.biz@gmail.com>
2024-09-19 20:39:07 +0200

    Zmiana znaku >= na >

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ console.log(fruits);
 const numbers = [5, 7, 19, 34, 21, 11, 0, 1];
 
 for (const number of numbers) {
-    if (number >= 5) {
+    if (number > 5) {
         console.log(number);
     }
 }
