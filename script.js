e316403bbcab556f1a99c88bef75d65a60992eca
Patryk Bielecki <bielecki.biz@gmail.com>
2024-09-19 20:35:52 +0200

    Initaial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const fruits = ["Jabłko", "Banan", "Pomarańcza", "Winogrona", "Arbuz"];
+
+console.log(fruits);
+
+const numbers = [5, 7, 19, 34, 21, 11, 0, 1];
+
+for (const number of numbers) {
+    if (number >= 5) {
+        console.log(number);
+    }
+}
